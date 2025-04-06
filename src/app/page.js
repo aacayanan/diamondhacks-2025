cc9ab33a910ee02b3112f050974a68da0d45105a
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2025-04-06 02:44:02 -0700

    session page interface is in progress. session page urls have unique ids

--- src/app/page.js
+++ src/app/page.js
@@ -8,21 +8,24 @@ import {useRouter} from 'next/navigation';
 
 export default function Home() {
     const router = useRouter();
+
     const handleClick = async () => {
         // insert a new row in Supabase
         const {data, error} = await supabase
             .from('sessions')
-            .insert({ 'user_id': '51e96e2e-8716-4dbe-b072-cee9b0a1c26c' });
+            .insert({ 'user_id': '51e96e2e-8716-4dbe-b072-cee9b0a1c26c' })
+            .select();
+        const session_id = data[0].id;
         if (error) {
             console.error('Supabase insert error: ', error);
         }
-        router.push('/Session');
+        router.push(`/Session/${session_id}`);
     }
+
 
     return (
         <div>
             <h1>Login Page</h1>
-            {/*<Link href="Session">Start Session</Link>*/}
             <button onClick={handleClick}>Start Session</button>
         </div>
     );
