abd080a13025c1bc64997f7cc9a1d1d6bbc558ce
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-04-06 03:34:31 -0700

    link backend to supabase after processing video

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,11 @@
 import os
-import tempfile
-
+from supabase import create_client, Client
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-import cv2
 from video_process import process_video
 
 app = Flask(__name__)
-
+load_dotenv()
 
 @app.route("/")
 def home():
@@ -19,6 +18,7 @@ def process():
         return jsonify(message="No file", status_code=400)
 
     video = request.files['blob']
+    session_id = request.form['session_id']
 
     # define the path where you want to save the video locally
     save_dir = './'
@@ -28,10 +28,19 @@ def process():
     # save the uploaded video file directly using FileStorage.save()
     video.save(save_path)
 
+    # now send to supabase
+    # supabase url and api key
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
     send_to_supabase = process_video(save_path)
 
-    # now send to supabase
-
+    data = {
+        'r_elbow_angle': send_to_supabase[0],
+        'l_elbow_angle': send_to_supabase[1]
+    }
+    response = supabase.table('sessions').update(data).eq('id', session_id).execute()
+    print(response)
     return f"Video saved and processed at: {save_path}", 200
 
 
